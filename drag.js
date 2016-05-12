ef2f9b92a879388b7dc7e409bbba302a6caf93c9
hongjingwu2016 <ww20160418@126.com>
2016-05-12 19:28:59 +0800

    提交个人BLOG框架

---
+++ drag.js
@@ -0,0 +1,11 @@
+window.onload=function(){
+	var oBox=document.getElementById('box');
+	oBox.onmousedown=function(ev){
+		var oEvent=ev||event;
+		var disX=oEvent.clientX-oBox.offetWidth;
+		var disY=oEvent.clientY-oBox.offetHeight;
+		document.onmousemove=function(ev){
+
+		}
+	}
+};
